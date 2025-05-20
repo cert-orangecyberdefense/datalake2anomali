8beb1c8d8a4a82325b351d08686e2ab8fcd68330
Sebastian Pitur <sebastian.pitur@orange.com>
2025-05-20 12:41:31 +0300

    make PR uniform with v2

--- src/models.py
+++ src/models.py
@@ -2,14 +2,18 @@ from pydantic import BaseModel
 
 
 
-class PatchTipReportModel(BaseModel):
+
+class BaseTipReportModel(BaseModel):
     body: str
     body_content_type: str = 'markdown'
     modified_ts: str
     name: str
     original_source: str = 'WorldWatch'
     source: str = "WorldWatch"
-    tags: list = []
 
-class AnomaliTipReportModel(PatchTipReportModel):
+class PatchTipReportModel(BaseTipReportModel):
+    tags_v2: list = []
+
+class AnomaliTipReportModel(BaseTipReportModel):
     created_ts: str
+    tags: list = []
